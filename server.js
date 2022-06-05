e1a699eef551b6c2aa8362cc15650819e7773152
Christopher Weiner <chriscweiner@gmail.com>
2022-06-05 16:21:08 -0700

    Rate Limiting: Got per-IP rate limiting implemented and testing, working as intended.
    Next is per-token/user rate limiting, idea is to use an if statement and checking the valid token before the if-statement.

--- server.js
+++ server.js
@@ -4,13 +4,60 @@
 
 const express = require('express')
 const morgan = require('morgan')
-
+const redis = require('redis')
 
 const { connectToDb } = require('./lib/mongo')
 const api = require('./api')
 
 const app = express()
 const port = process.env.PORT || 8000
+
+const redisHost = process.env.REDIS_HOST || 'localhost'
+const redisPort = process.env.REDIS_PORT || 6379
+
+const redisClient = redis.createClient(redisHost,redisPort)
+
+const rateLimitMaxRequestWithAuth = 30
+const rateLimitMaxRequestWithoutAuth = 10
+const rateLimitWindowMs = 60000
+
+async function rateLimit(req,res,next){
+  const ip = req.ip
+  //const 
+  let tokenBucket
+  try{
+    tokenBucket = await redisClient.hGetAll(ip)
+  }catch(e){
+    next()
+    return
+  }
+  console.log("== tokenBucket before:",tokenBucket)
+  tokenBucket = {
+    tokens: parseFloat(tokenBucket.tokens)|| rateLimitMaxRequestWithoutAuth,
+    last: parseInt(tokenBucket.last)||Date.now()
+  }
+  console.log("== tokenBucket after:",tokenBucket)
+
+  const now = Date.now()
+  const ellapsedMs = now - tokenBucket.last
+  tokenBucket.tokens +=ellapsedMs*(rateLimitMaxRequestWithoutAuth/rateLimitWindowMs)
+  tokenBucket.tokens = Math.min(rateLimitMaxRequestWithoutAuth,tokenBucket.tokens)
+  tokenBucket.last = now
+
+  if(tokenBucket.tokens >=1){
+    tokenBucket.tokens -=1
+    await redisClient.hSet(ip,[['tokens',tokenBucket.tokens],['last',tokenBucket.last]])
+    next()
+  }else{
+    await redisClient.hSet(ip,[['tokens',tokenBucket.tokens],['last',tokenBucket.last]])
+    res.status(429).send({
+      err:"Too many requests per minute"
+    })
+  }
+}
+
+
+app.use(rateLimit)
 
 /*
  * Morgan is a popular logger.
@@ -44,8 +91,9 @@ app.use('*', function (err, req, res, next) {
   })
 })
 
-connectToDb(function () {
+redisClient.connect().then(connectToDb(function () {
   app.listen(port, function () {
     console.log("== Server is running on port", port)
   })
 })
+)
